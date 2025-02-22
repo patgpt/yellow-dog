8741314a1fa56f7e3745953595c750cd45dcb739
Patrick Klly <doitformurphy@gmail.com>
2025-02-21 22:31:34 -0500

    Refactor ESLint configuration to remove unused plugins and streamline setup

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,4 @@
-import css from '@eslint/css'
 import { FlatCompat } from '@eslint/eslintrc'
-import markdown from '@eslint/markdown'
 
 const compat: FlatCompat = new FlatCompat({
   baseDirectory: import.meta.dirname
@@ -9,8 +7,6 @@ const compat: FlatCompat = new FlatCompat({
 const eslintConfig = [
   ...compat.config({
     extends: ['next/core-web-vitals', 'next/typescript', 'prettier']
-  }),
-  css.configs.recommended,
-  markdown.configs.recommended
+  })
 ]
 export default eslintConfig
