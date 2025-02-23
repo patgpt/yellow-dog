14b6f233f281bb4f058d3441d54501989e1d561f
Patrick Klly <doitformurphy@gmail.com>
2025-02-23 17:35:37 -0500

    Add initial setup files, configurations, and components for the application

--- src/app/(site)/[locale]/not-found.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { useTranslations } from 'next-intl'
-
-export default function NotFound() {
-  const t = useTranslations('common')
-  return <div>{t('notFound')}</div>
-}
--- src/app/(site)/[locale]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { Link } from '@/i18n/routing'
-import { useTranslations } from 'next-intl'
-export default function Page() {
-  const t = useTranslations('common')
-  return <Link href="/">{t('home')}</Link>
-}
