4b1c574a54fab5689cd8f98ff214ae4271d91e09
Patrick Klly <doitformurphy@gmail.com>
2025-02-23 19:04:31 -0500

    Remove unused files and configurations to clean up the project

---
+++ .next/server/vendor-chunks/to-snake-case.js
@@ -0,0 +1,24 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/to-snake-case";
+exports.ids = ["vendor-chunks/to-snake-case"];
+exports.modules = {
+
+/***/ "(rsc)/./node_modules/to-snake-case/index.js":
+/*!*********************************************!*\
+  !*** ./node_modules/to-snake-case/index.js ***!
+  \*********************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("\nvar toSpace = __webpack_require__(/*! to-space-case */ \"(rsc)/./node_modules/to-space-case/index.js\")\n\n/**\n * Export.\n */\n\nmodule.exports = toSnakeCase\n\n/**\n * Convert a `string` to snake case.\n *\n * @param {String} string\n * @return {String}\n */\n\nfunction toSnakeCase(string) {\n  return toSpace(string).replace(/\\s/g, '_')\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHJzYykvLi9ub2RlX21vZHVsZXMvdG8tc25ha2UtY2FzZS9pbmRleC5qcyIsIm1hcHBpbmdzIjoiO0FBQ0EsY0FBYyxtQkFBTyxDQUFDLGtFQUFlOztBQUVyQztBQUNBO0FBQ0E7O0FBRUE7O0FBRUE7QUFDQTtBQUNBO0FBQ0EsV0FBVyxRQUFRO0FBQ25CLFlBQVk7QUFDWjs7QUFFQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIi9Vc2Vycy93YXZlZnVuY3Rpb24veWVsbG93ZG9nL25vZGVfbW9kdWxlcy90by1zbmFrZS1jYXNlL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIlxudmFyIHRvU3BhY2UgPSByZXF1aXJlKCd0by1zcGFjZS1jYXNlJylcblxuLyoqXG4gKiBFeHBvcnQuXG4gKi9cblxubW9kdWxlLmV4cG9ydHMgPSB0b1NuYWtlQ2FzZVxuXG4vKipcbiAqIENvbnZlcnQgYSBgc3RyaW5nYCB0byBzbmFrZSBjYXNlLlxuICpcbiAqIEBwYXJhbSB7U3RyaW5nfSBzdHJpbmdcbiAqIEByZXR1cm4ge1N0cmluZ31cbiAqL1xuXG5mdW5jdGlvbiB0b1NuYWtlQ2FzZShzdHJpbmcpIHtcbiAgcmV0dXJuIHRvU3BhY2Uoc3RyaW5nKS5yZXBsYWNlKC9cXHMvZywgJ18nKVxufVxuIl0sIm5hbWVzIjpbXSwiaWdub3JlTGlzdCI6WzBdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(rsc)/./node_modules/to-snake-case/index.js\n");
+
+/***/ })
+
+};
+;
--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250223_213549_initial from './20250223_213549_initial';
-
-export const migrations = [
-  {
-    up: migration_20250223_213549_initial.up,
-    down: migration_20250223_213549_initial.down,
-    name: '20250223_213549_initial'
-  },
-];
